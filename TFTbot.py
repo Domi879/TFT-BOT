aad9f358cc84f6a13aa5cdbe27ead70141c3b834
Domi <giacomotassoni8@gmail.coim>
2025-01-17 21:12:19 +0100

    secondo commit

--- TFTbot.py
+++ TFTbot.py
@@ -2,17 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
+import threading
+from flask import Flask
 
 # Inserisci il tuo token qui
 TELEGRAM_BOT_TOKEN = "7473377342:AAGbCvzSMxAhCeb_m9wuF9VSsLC4BmzOPT0"  # Sostituisci con il tuo token
-
 # URL della pagina di news di TFT in italiano
 TFT_URL = "https://euw.leagueoflegends.com/it-it/news/tags/teamfight-tactics/"
-
 # URL di una foto che vuoi inviare insieme alla patch
 IMAGE_URL = "https://i.imgur.com/KyA7abw.jpeg"  # Cambia con l'URL dell'immagine
-
 # File dove salviamo i chat_id dei gruppi
 CHAT_ID_FILE = "chat_ids.txt"
 
@@ -64,10 +62,8 @@ async def send_patch(application: Application):
     if patch_url:
         with open(CHAT_ID_FILE, "r") as file:
             chat_ids = file.readlines()
-
         # Rimuovi eventuali righe vuote
         chat_ids = [chat_id.strip() for chat_id in chat_ids]
-
         # Invia il messaggio con la foto come allegato e il link della patch a ogni chat_id
         for chat_id in chat_ids:
             try:
@@ -78,20 +74,38 @@ async def send_patch(application: Application):
             except Exception as e:
                 print(f"Errore durante l'invio al gruppo {chat_id}: {e}")
 
+# Crea un'app Flask per il controllo di salute
+app = Flask(__name__)
+
+# Endpoint di salute per il controllo su Koyeb
+@app.route("/health", methods=["GET"])
+def health_check():
+    return "OK", 200
+
+# Funzione per avviare il server Flask in un thread separato
+def run_flask():
+    app.run(host="0.0.0.0", port=8000)
 # Funzione principale che avvia il bot
 def main():
-    # Crea l'applicazione
+    # Avvia il server Flask in un thread separato per il controllo di salute
+    thread = threading.Thread(target=run_flask)
+    thread.daemon = True
+    thread.start()
+
+    # Crea l'applicazione Telegram
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     # Aggiungi gli handler per i comandi
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('patch', patch))
 
-# Esegui il monitoraggio della patch ogni 30 minuti
+    # Esegui il monitoraggio della patch ogni 30 minuti
     job_queue = application.job_queue
     job_queue.run_repeating(lambda context: send_patch(application), interval=1800, first=0)  # Controlla ogni 30 minuti
 
-    # Avvia il bot
+    # Avvia il bot in modalità polling
     application.run_polling()
+
+# Avvia il bot
 if __name__ == "__main__":
     main()
